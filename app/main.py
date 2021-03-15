ff88b79130e3b6d050d67bab4524911f2a0279e6
Hemicharly <hemicharlythiago@gmail.com>
2021-03-14 21:57:19 -0300

    feat: add docker

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import pyaudio
 import pyttsx3
 import json
 import core
-from nlu.classifier import classify
+# from nlu.classifier import classify
 
 # Síntese de fala
 engine = pyttsx3.init()
@@ -15,34 +15,34 @@ voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[-2].id)
 
 
-def speak(text):
-    engine.say(text)
-    engine.runAndWait()
+# def speak(text):
+#     engine.say(text)
+#     engine.runAndWait()
 
 
-def evaluate(text):
-    # Reconhecer entidade do texto. 
-    entity = classify(text)
-    if entity == 'time|getTime':
-        speak(core.SystemInfo.get_time())
-    elif entity == 'time|getDate':
-        speak(core.SystemInfo.get_date())
-
-    # Abrir programas
-    elif entity == 'open|notepad':
-        speak('Abrindo o bloco de notas')
-        os.system('notepad.exe')
-    elif entity == 'open|chrome':
-        speak('Abrindo o google chrome')
-        os.system('"C:/Program Files/Google/Chrome/Application/chrome.exe"')
-
-    print('Text: {}  Entity: {}'.format(text, entity))
+# def evaluate(text):
+#     # Reconhecer entidade do texto.
+#     entity = classify(text)
+#     if entity == 'time|getTime':
+#         speak(core.SystemInfo.get_time())
+#     elif entity == 'time|getDate':
+#         speak(core.SystemInfo.get_date())
+#
+#     # Abrir programas
+#     elif entity == 'open|notepad':
+#         speak('Abrindo o bloco de notas')
+#         os.system('notepad.exe')
+#     elif entity == 'open|chrome':
+#         speak('Abrindo o google chrome')
+#         os.system('"C:/Program Files/Google/Chrome/Application/chrome.exe"')
+#
+#     print('Text: {}  Entity: {}'.format(text, entity))
 
 
 # Reconhecimento de fala
 
-model = Model('model')
-rec = KaldiRecognizer(model, 16000)
+# model = Model('model')
+# rec = KaldiRecognizer(model, 16000)
 
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=2048)
@@ -53,10 +53,10 @@ while True:
     data = stream.read(2048)
     if len(data) == 0:
         break
-    if rec.AcceptWaveform(data):
-        result = rec.Result()
-        result = json.loads(result)
-
-        if result is not None:
-            text = result['text']
-            evaluate(text)
+    # if rec.AcceptWaveform(data):
+    #     result = rec.Result()
+    #     result = json.loads(result)
+    #
+    #     if result is not None:
+    #         text = result['text']
+    #         evaluate(text)
